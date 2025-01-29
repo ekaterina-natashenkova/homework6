fd07bf3a6a126b72f768819ff0348cdf5a7dffd3
EKATERINA NATASHENKOVA <vasilieva-ekaterina@yandex.ru>
2025-01-29 22:03:06 +0300

    homework done - corrected

--- src/Main.java
+++ src/Main.java
@@ -13,77 +13,78 @@ public class Main {
         System.out.println("\nзадача 1");
         int moneybox = 15000;
         int total = 0;
-        int i = 1;
+        int i = 0;
         while (total < 2_459_000) {
             total = total + moneybox;
             i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
-        System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        //я поняла задание так, что должна быть выведена 1 сточка с номером месяца, когда накопится требуемая сумма
 
         System.out.println("\nзадача 2");
         int a = 1;
-        int b = 10;
         while (a <= 10) {
             System.out.print(a + " ");
             a++;
         }
         System.out.println();
-        while (b >= 1) {
+        for (int b = 10; b >= 1; b--) {
             System.out.print(b + " ");
-            b--;
         }
+        //не заметила в условии, что обратная последовательность выводится с использованием цикла for
 
         System.out.println("\n\nзадача 3");
         int polulationY = 12_000_000;
         int born = 17;
         int die = 8;
-        int year = 0;
+        int year = 1;
         while (year <= 10) {
+            polulationY = polulationY + polulationY * born / 1000 - polulationY * die / 1000;
             System.out.println("Год " + year + ", численность населения составляет " + polulationY);
             year++;
-            polulationY = polulationY + ((polulationY * born) / 1000) + ((polulationY * die) / 1000);
         }
+        // я поняла задание так, что должно быть выведено 10 строк, что и сделано. Вывод в нулевом году убрала
 
         System.out.println("\nзадача 4");
-        int bank = 15000;
         int deposit = 15000;
         int j = 0;
         while (deposit < 12_000_000) {
             deposit = deposit + ((deposit / 100) * 7);
-            deposit = deposit + bank;
             j++;
             System.out.println("Месяц " + j + ", сумма накоплений равна " + deposit + " рублей");
         }
         System.out.println("Итого " + deposit + " рублей");
+        // перечитала условия - нужно посчитать накопление только за счет %
 
         System.out.println("\nзадача 5");
-        int bank2 = 15000;
-        int deposit2 = 15000;
-        int j2 = 0;
-        while (deposit2 < 12_000_000) {
-            deposit2 = deposit2 + ((deposit2 / 100) * 7);
-            deposit2 = deposit2 + bank2;
-            j2++;
-            if (j2 % 6 == 0) {
-                System.out.println("Месяц " + j2 + ", сумма накоплений равна " + deposit2 + " рублей");
+        deposit = 15000;
+        j = 0;
+        while (deposit < 12_000_000) {
+            deposit = deposit + ((deposit / 100) * 7);
+            j++;
+            if (j % 6 == 0) {
+                System.out.println("Месяц " + j + ", сумма накоплений равна " + deposit + " рублей");
             }
         }
-        System.out.println("Итого " + deposit2 + " рублей");
+        System.out.println("Итого " + deposit + " рублей");
+        // перечитала условия - нужно посчитать накопление только за счет %
 
         System.out.println("\nзадача 6");
-        int bank3 = 15000;
-        int deposit3 = 15000;
-        int j3 = 9 * 12;
+        deposit = 15000;
+        int montgh = 9 * 12;
         int n = 0;
-        while (j3 <= 108 && j3 > 0) {
-            deposit3 = deposit3 + ((deposit3 / 100) * 7);
-            deposit3 = deposit3 + bank3;
-            j3--;
+        while (montgh <= 108 && montgh > 0) {
+            deposit = deposit + deposit / 100 * 7;
+            montgh--;
             n++;
-            if (j3 % 6 == 0) {
-                System.out.println("Месяц " + n + ", сумма накоплений равна " + deposit3 + " рублей");
+            if (montgh % 6 == 0) {
+                System.out.println("Сумма накоплений за " + n + " месяцев равна " + deposit + " рублей");
             }
         }
+        System.out.println("Итого, сумма накоплений за 9 лет " + deposit + " рублей");
+        // перечитала условия - нужно посчитать накопление только за счет %
+        // выводится сумма за каждый 6й месяц с накопительным итогом
+        // скорректировала сообщение для вывода в консоль и добавила итоговую строку
 
         System.out.println("\nзадача 7");
         int dayNumber = 3;
@@ -94,10 +95,10 @@ public class Main {
 
         System.out.println("\nзадача 8");
         int t = 0;
+        int comet = 79;
         int yearT = 2024;
         int t1 = yearT - 200;
         int t2 = yearT + 100;
-        int comet = 79;
         while (t < t2) {
             t = t + comet;
             if (t > t1 && t < t2) {
